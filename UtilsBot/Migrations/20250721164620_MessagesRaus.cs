d4afb47b33fec12f0a4e43474e4f689c0c1fe4a5
LuGeNull <l.gecas@protonmail.com>
2025-07-21 20:10:33 +0200

    _
    Signed-off-by: LuGeNull <l.gecas@protonmail.com>

---
+++ UtilsBot/Migrations/20250721164620_MessagesRaus.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UtilsBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessagesRaus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BenachrichtigenZeitBis",
+                table: "AllgemeinePerson");
+
+            migrationBuilder.DropColumn(
+                name: "BenachrichtigenZeitVon",
+                table: "AllgemeinePerson");
+
+            migrationBuilder.DropColumn(
+                name: "WillBenachrichtigungenBekommen",
+                table: "AllgemeinePerson");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "BenachrichtigenZeitBis",
+                table: "AllgemeinePerson",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "BenachrichtigenZeitVon",
+                table: "AllgemeinePerson",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WillBenachrichtigungenBekommen",
+                table: "AllgemeinePerson",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
