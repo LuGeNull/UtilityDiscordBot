556015b1d288bfbeaf224fd97aa16873cd843e92
LuGeNull <l.gecas@protonmail.com>
2025-11-15 15:12:32 +0100

    _

---
+++ UtilsBot/Migrations/20251016164948_BetRemoved.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UtilsBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class BetRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Placements");
+
+            migrationBuilder.DropTable(
+                name: "Bet");
+
+            migrationBuilder.DropColumn(
+                name: "Gold",
+                table: "AllgemeinePerson");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Gold",
+                table: "AllgemeinePerson",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Bet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ChannelId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    EndedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Ereignis1Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Ereignis2Name = table.Column<string>(type: "TEXT", nullable: false),
+                    MaxPayoutMultiplikator = table.Column<int>(type: "INTEGER", nullable: false),
+                    MessageId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    ReferenzId = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    UserIdStartedBet = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    WetteWurdeAbgebrochen = table.Column<bool>(type: "INTEGER", nullable: false),
+                    WetteWurdeBeendet = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Placements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BetId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    GoldRefunded = table.Column<long>(type: "INTEGER", nullable: false),
+                    GoldWon = table.Column<long>(type: "INTEGER", nullable: false),
+                    Site = table.Column<bool>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<ulong>(type: "INTEGER", nullable: true),
+                    betAmount = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Placements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Placements_Bet_BetId",
+                        column: x => x.BetId,
+                        principalTable: "Bet",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Placements_BetId",
+                table: "Placements",
+                column: "BetId");
+        }
+    }
+}
